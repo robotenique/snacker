82058a30b6b999adba6fc39cba8d0ff1aa5f1723
Jayde Yue <JaydeYue@jaydeyue.com>
2018-10-26 22:11:49 -0400

    search reviews and snacks based on custom filters are done and tested

--- snacker/flaskr/app.py
+++ snacker/flaskr/app.py
@@ -4,11 +4,11 @@ import urllib
 import mongoengine as mg
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_bcrypt import Bcrypt
-from flask_table import *
 from werkzeug.contrib.fixers import ProxyFix
 
 from forms import RegistrationForm, LoginForm
 from schema import *
+from util import *
 
 """
 You need to create a mongo account and let Jayde know your mongo email address to add you to the db system
@@ -118,7 +118,6 @@ def hello_world():
 
     # Test Review
     # Display existing reviews in the db
-    print(f"a\n", file=sys.stdout)
     for obj in Review.objects:
         print(f"    Before Save Review: {obj.user_id} {obj.snack_id} {obj.description}\n", file=sys.stdout)
     review = Review(user_id="5bd1377387bec222cc6e6026", snack_id="5bd1377387bec222cc6e6027", description="like it",
@@ -198,89 +197,92 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route("/snack_reviews/<string:snack_id>", methods=['GET'])
-def find_reviews_for_snack(snack_id):
+# Finished and tested
+@app.route("/snack_reviews/<string:filters>", methods=['GET'])
+def find_reviews_for_snack(filters):
     """
-    Find all the reviews for a snack.
-    To display a table of reviews for a snack_id, enter local/_url/snack_reviews/snack_id in your browser.
+    Find all reviews given filter
+    For overall rating, and the metrics, all reviews with greater or equal to the given value will be returned
+    Results currently ordered by descending overall rating
+    /snack_reviews/snack_id=abc+overall_rating=3...
     """
-    reviews = Review.objects(snack_id=snack_id)
-    print(f"snack_reviews: {reviews}", file=sys.stdout)
-    if not reviews:
-        return "No reviews found"
-    else:
-        display = ReviewResults(reviews)
-        display.border = True
-        return render_template('reviews_for_snack.html', table=display)
+    all_filters = filters.split("+")
+    print(f"{all_filters}\n", file=sys.stdout)
+    queryset = Review.objects
+    # all reviews will be returned if nothing specified
+    if "=" in filters:
+        for individual_filter in all_filters:
+            this_filter = individual_filter.split("=")
+            if this_filter[0] == "user_id":
+                queryset = queryset.filter(user_id=this_filter[1])
+            elif this_filter[0] == "snack_id":
+                queryset = queryset.filter(snack_id=this_filter[1])
+            elif this_filter[0] == "overall_rating":
+                queryset = queryset.filter(overall_rating__gte=this_filter[1])
+            elif this_filter[0] == "geolocation":
+                queryset = queryset.filter(geolocation=this_filter[1])
+            elif this_filter[0] == "sourness":
+                queryset = queryset.filter(sourness__gte=this_filter[1])
+            elif this_filter[0] == "spiciness":
+                queryset = queryset.filter(spiciness__gte=this_filter[1])
+            elif this_filter[0] == "bitterness":
+                queryset = queryset.filter(bitterness__gte=this_filter[1])
+            elif this_filter[0] == "sweetness":
+                queryset = queryset.filter(sweetness__gte=this_filter[1])
+            elif this_filter[0] == "saltiness":
+                queryset = queryset.filter(saltiness__gte=this_filter[1])
+    queryset = queryset.order_by("-overall_rating")
+    print(f"snack_reviews: {queryset}", file=sys.stdout)
+    display = ReviewResults(queryset)
+    display.border = True
+    # Return results in a table, the metrics such as sourness are not displayed because if they are null, they give
+    #   the current simple front end table an error, but it is there for use
+    return render_template('reviews_for_snack.html', table=display)
 
 
-# TODO: remove after implementing the front end.
-class ReviewResults(Table):
-    """
-    Test class that helps to display the readable snacks data to the front end in the format of a table.
-    It allows us to test the backend and html rendering, without having to wait for the implementation
-    of the final front end.
-    """
-    id = Col('Review Id')
-    user_id = Col('User ID')
-    snack_id = Col('Snack ID')
-    description = Col('Description')
-    overall_rating = Col('Overall Rating')
-    geolocation = Col('Geolocation')
-    sourness = Col('sourness')
-    spiciness = Col('spiciness')
-    bitterness = Col('bitterness')
-    sweetness = Col('sweetness')
-    saltiness = Col('saltiness')
-
-
-# TODO: test this
-@app.route("/find_snacks?<string:filter>", methods=['GET'])
+# Finished and tested
+@app.route("/find_snacks/<string:filters>", methods=['GET'])
 def find_snack_by_filter(filters):
     """
-    Find all snacks given a filter
-    /find_snacks?snack_name=abc&available_at_locations=a+b+c&...
+    Find all snacks given filter
+    Only support searching for one location at a time now
+    For is verfied, false for false and true for true
+    Results currently ordered by snack name
+    /find_snacks/snack_name=abc+available_at_locations=a+...
+    /find_snacks/all if we want to get all snacks
     """
-    filter_query = {}
-    all_filters = filters.split("&")
-    available_basic_filters = ['snack_name', 'snack_brand', 'snack_company_name', 'is_verified', 'category']
+    all_filters = filters.split("+")
+    print(f"{all_filters}\n", file=sys.stdout)
+    queryset = Snack.objects
 
-    for individual_filter in all_filters:
-        filter = individual_filter.split("=")
-        filter_name = filter[0]
-        filter_variable = filter[1]
-        if filter_name in available_basic_filters:
-            filter_query[filter_name] = filter_variable
-        elif filter_name == "available_at_locations":
-            filter_query['available_at_locations'] = {"$elemMatch": filter_variable}
-
-    snacks = Snack.objects.find(filter_query)
-    print(f"snack_reviews: {snacks}", file=sys.stdout)
-    if not snacks:
-        return "No snacks founds"
-    else:
-        display = SnackResults(snacks)
-        display.border = True
-        # Return the same template as for the review, since it only needs to display a table.
-        return render_template('reviews_for_snack.html', table=display)
-
-
-# TODO: remove after implementing the front end.
-class SnackResults(Table):
-    """
-    Test class that helps to display the readable snacks data to the front end in the format of a table.
-    It allows us to test the backend and html rendering, without having to wait for the implementation
-    of the final front end.
-    """
-    id = Col('Snack Id')
-    snack_name = Col('snack_name')
-    available_at_locations = Col('Location')
-    snack_brand = Col('Brand')
-    snack_company_name = Col('Company Name')
-    photo_files = Col('Photo')
-    description = Col('Description')
-    is_verified = Col('if verified')
-    category = Col('Category')
+    # the search string should be all if we want to get all snacks, but we can type anything that doesn't include = to
+    #   get the same results
+    if "=" in filters:
+        for individual_filter in all_filters:
+            this_filter = individual_filter.split("=")
+            if this_filter[0] == "snack_name":
+                queryset = queryset.filter(snack_name=this_filter[1])
+            elif this_filter[0] == "available_at_locations":
+                # Note for this, say if they enter n, they will still return snacks in Canada because their contains
+                #   is based on string containment
+                queryset = queryset.filter(available_at_locations__contains=this_filter[1])
+            elif this_filter[0] == "snack_brand":
+                queryset = queryset.filter(snack_brand=this_filter[1])
+            elif this_filter[0] == "snack_company_name":
+                queryset = queryset.filter(snack_company_name=this_filter[1])
+            elif this_filter[0] == "is_verified":
+                if this_filter[1] == "false":
+                    queryset = queryset.filter(is_verified=False)
+                else:
+                    queryset = queryset.filter(is_verified=True)
+            elif this_filter[0] == "category":
+                queryset = queryset.filter(category=this_filter[1])
+    queryset = queryset.order_by("snack_name")
+    print(f"snack_reviews: {queryset}", file=sys.stdout)
+    display = SnackResults(queryset)
+    display.border = True
+    # Return the same template as for the review, since it only needs to display a table.
+    return render_template('reviews_for_snack.html', table=display)
 
 
 if __name__ == '__main__':
