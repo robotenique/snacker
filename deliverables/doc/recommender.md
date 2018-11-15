25c526136c2c2894d7bf2d79eee1a9e56effaf28
robotenique <julianogarcia_1997@hotmail.com>
2018-11-15 18:00:05 -0500

    refactor recommender_experiments.py and add more info for local mongodb shell in the docs

--- deliverables/doc/recommender.md
+++ deliverables/doc/recommender.md
@@ -3,8 +3,19 @@ Recommender System dev - Snacker
 Do experiments in the file recommender_experiments.py.
 Use a **local** database! For that, make sure you've downloaded mongodb,
 and use this to save and restore databases:
-- Save a database to a dump: [mongodump](https://docs.mongodb.com/manual/reference/program/mongodump/#bin.mongodump)
+- Save a database to a dump: [mongodump](https://docs.mongodb.com/manual/reference/program/mongodump/#bin.mongodump):
+```bash
+$ mongodump --uri "mongodb+srv://Jayde:Jayde@csc301-v3uno.mongodb.net/test?retryWrites=true"
+```
 - Restore a database from a dump: [mongorestore](https://docs.mongodb.com/manual/reference/program/mongorestore/#bin.mongorestore)
+
+* Start a localdatabase
+
+```bash
+$ sudo service mongod start
+```
+
+* Enter the shell with ```bash $ mongo ``` , check commands for reference [here](https://docs.mongodb.com/manual/reference/mongo-shell/)
 
 
 ## Recommender System - Approach
--- snacker/flaskr/recommender_experiments.py
+++ snacker/flaskr/recommender_experiments.py
@@ -1,10 +1,6 @@
 import urllib
-from flask import Flask, render_template, request, flash, redirect, url_for, make_response, Response
 from mongoengine import connect
 from mongoengine.queryset.visitor import Q
-from werkzeug.contrib.fixers import ProxyFix
-
-from forms import RegistrationForm, LoginForm, CreateReviewForm, CreateSnackForm
 import schema
 """
 The purpose of this file is to be used for experiments for the recommendation
@@ -12,37 +8,27 @@ algorithm of our application.
 
 Important: Check the docs in deliverable/doc/recommender.md
 """
-app = Flask(__name__)
 
-# With these constant strings, we can connect to generic databases
-USERNAME_FILE = "username.txt"
-PASSWORD_FILE = "password.txt"
-MONGO_SERVER = "csc301-v3uno.mongodb.net"
-APP_NAME = "Snacker"
+def recsys():
+    DATABASE = "test"
+    my_db = mongo[DATABASE]
+    print(f"Collections found in the current database: {my_db.collection_names()}")
+    generate_training_data(my_db)
 
-
-try:
-    print("hello")
-    #mongo_uri = f"mongodb+srv://Jayde:Jayde@csc301-v3uno.mongodb.net/test?retryWrites=true"
-    mongo_uri = "mongodb://localhost:27017/"
-    app.config["MONGO_URI"] = mongo_uri
-    mongo = connect(host=mongo_uri)
-    # This is necessary for user tracking
-    app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
-except Exception as inst:
-    raise Exception("Error in database connection:", inst)
-
-# TODO: Need to change this to an env variable later
-app.config["SECRET_KEY"] = "2a0ca44c88db3d509085f32f2d4ed2e6"
-app.config['DEBUG'] = True
-app.url_map.strict_slashes = False
-
-
-@app.route('/rec_test')
-def rec_test():
-	DATABASE = "test"
-	my_db = mongo[DATABASE]
-    return "Recommender test"
+def generate_training_data(my_db):
+    """Given a database, generate training data from that specific database
+       and return the data generated"""
+    collection_names = my_db.collection_names()
+    assert "snack" in collection_names
+    assert "review" in collection_names
+    assert "user" in collection_names
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        mongo_uri = "mongodb://localhost:27017/"
+        mongo = connect(host=mongo_uri)
+        print("Connected to mongo db...\n")
+    except Exception as inst:
+        raise Exception("Error in database connection:", inst)
+    recsys()
+
