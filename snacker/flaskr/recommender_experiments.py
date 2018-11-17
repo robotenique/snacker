cf4f7545785597472d436da9e4210b09461ec1d1
robotenique <julianogarcia_1997@hotmail.com>
2018-11-16 23:10:07 -0500

    add Matrix Factorization that deals with missing value

--- snacker/flaskr/recommender_experiments.py
+++ snacker/flaskr/recommender_experiments.py
@@ -7,11 +7,103 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
 """
-The purpose of this file is to be used for experiments for the recommendation
-algorithm of our application.
+The purpose of this file is to be used for the recommendation
+algorithm training of our application.
 
 Important: Check the docs in deliverable/doc/recommender.md
 """
+
+# MF class from A. Yeung, at http://www.albertauyeung.com/post/python-matrix-factorization/
+class MF(object):
+    def __init__(self, R, K, alpha, beta, iterations):
+        """
+        Perform matrix factorization to predict empty
+        entries in a matrix.
+
+        Arguments
+        - R (ndarray)   : user-item rating matrix
+        - K (int)       : number of latent dimensions
+        - alpha (float) : learning rate
+        - beta (float)  : regularization parameter
+        """
+
+        self.R = R
+        self.num_users, self.num_items = R.shape
+        self.K = K
+        self.alpha = alpha
+        self.beta = beta
+        self.iterations = iterations
+
+    def train(self):
+        # Initialize user and item latent feature matrice
+        self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
+        self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
+
+        # Initialize the biases
+        self.b_u = np.zeros(self.num_users)
+        self.b_i = np.zeros(self.num_items)
+        self.b = np.mean(self.R[np.where(self.R != 0)])
+
+        # Create a list of training samples
+        self.samples = [
+            (i, j, self.R[i, j])
+            for i in range(self.num_users)
+            for j in range(self.num_items)
+            if self.R[i, j] > 0
+        ]
+
+        # Perform stochastic gradient descent for number of iterations
+        training_process = []
+        for i in range(self.iterations):
+            np.random.shuffle(self.samples)
+            self.sgd()
+            mse = self.mse()
+            training_process.append((i, mse))
+            if (i+1) % 10 == 0:
+                print("Iteration: %d ; error = %.4f" % (i+1, mse))
+
+        return training_process
+
+    def mse(self):
+        """
+        A function to compute the total mean square error
+        """
+        xs, ys = self.R.nonzero()
+        predicted = self.full_matrix()
+        error = 0
+        for x, y in zip(xs, ys):
+            error += pow(self.R[x, y] - predicted[x, y], 2)
+        return np.sqrt(error)
+
+    def sgd(self):
+        """
+        Perform stochastic gradient descent
+        """
+        for i, j, r in self.samples:
+            # Computer prediction and error
+            prediction = self.get_rating(i, j)
+            e = (r - prediction)
+
+            # Update biases
+            self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
+            self.b_i[j] += self.alpha * (e - self.beta * self.b_i[j])
+
+            # Update user and item latent feature matrices
+            self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
+            self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
+
+    def get_rating(self, i, j):
+        """
+        Get the predicted rating of user i and item j
+        """
+        prediction = self.b + self.b_u[i] + self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
+        return prediction
+
+    def full_matrix(self):
+        """
+        Computer the full matrix using the resultant biases, P and Q
+        """
+        return self.b + self.b_u[:,np.newaxis] + self.b_i[np.newaxis:,] + self.P.dot(self.Q.T)
 
 def recsys():
     DATABASE = "test"
@@ -118,8 +210,9 @@ def training_recc_engine(train_data, K=2, train_size=.70):
     X_train = rnd_permutation[:last_pos]
     X_test = rnd_permutation[last_pos:]
     new = np.zeros((num_users, num_snacks))
-    # Populate our matrix with ONLY training data. The rest is 0 ==> Not available
+    # Populate our matrix with ONLY training data. The rest is 0 (this means it's Not available)
     new[X_train[:, 0].astype(int), X_train[:, 1].astype(int)] = X_train[:, 2]
+    
     #X_sparse = training_data["X_sparse"]
 
 if __name__ == '__main__':
